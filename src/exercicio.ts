19392034a03ed14f1c990c6c2c0d225e632fb0a8
Manoel Paulo <paulo.neto.jp@hotmail.com>
2025-06-30 15:35:07 -0300

    inicial commit

---
+++ src/exercicio.ts
@@ -0,0 +1,6 @@
+const calcula = (num1: number, num2: number): number => num1 * num2;
+
+const dizoi = (pessoa: string): string => 'Ola ' + pessoa;
+
+console.log(calcula(3,4));
+console.log(dizoi('Manoel'));
