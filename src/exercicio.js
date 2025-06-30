cb770f6e052c29a95b3ca160d95699a2af92fd7c
Manoel Paulo <paulo.neto.jp@hotmail.com>
2025-06-30 15:38:52 -0300

    string fixed

--- src/exercicio.js
+++ src/exercicio.js
@@ -1,5 +1,5 @@
 "use strict";
 const calcula = (num1, num2) => num1 * num2;
-const dizoi = (pessoa) => 'Ola ' + pessoa;
+const dizoi = (pessoa) => `Ola  ${pessoa}`;
 console.log(calcula(3, 4));
 console.log(dizoi('Manoel'));
